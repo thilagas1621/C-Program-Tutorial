1af985f004e768f94915be3b2f07812416e7eaf5
thilagas1621 <109333087+thilagas1621@users.noreply.github.com>
2022-10-20 15:27:30 +0530

    Add files via upload

---
+++ 2_Datatypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    char b='$';
+    float c=12.5f;
+    long int d=2582l;
+    int aa=0x41;
+    float cc=253e-2f;
+    printf("%d \n %d \n%0.2f",a,b,c);
+    return 0;
+}
